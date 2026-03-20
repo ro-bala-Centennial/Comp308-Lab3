d59023e28e566fcb8b790569aa3cdaa0276939a4
Abraar Sadek <abraarsadek@gmail.com>
2026-03-20 17:29:53 -0400

    Building shell App

--- frontend/shell/vite.config.js
+++ frontend/shell/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "shell",
+      remotes: {
+        auth_mf: "http://localhost:5174/assets/remoteEntry.js",
+        community_mf: "http://localhost:5175/assets/remoteEntry.js",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  server: {
+    port: 5173,
+  },
+  build: {
+    target: "esnext",
+  },
+});
