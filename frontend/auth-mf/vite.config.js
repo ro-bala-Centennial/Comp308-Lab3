34d5b16522373c44e43b3a361f0e4f438cc0210f
Abraar Sadek <abraarsadek@gmail.com>
2026-03-20 17:03:02 -0400

    Configuring Federation For auth-mf

--- frontend/auth-mf/vite.config.js
+++ frontend/auth-mf/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "auth_mf",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./AuthApp": "./src/App.jsx",
+      },
+      shared: ["react", "react-dom", "@apollo/client", "graphql"],
+    }),
+  ],
+  server: {
+    port: 5174,
+  },
+  build: {
+    target: "esnext",
+  },
+});
